ac1134d82e1dc3d6ac053638e70db1cc69132934
AngelD3Jesus <angel_3012230140@utd.edu.mx>
2025-04-06 17:27:43 -0600

    Subiendo microservicio clientes

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import bodyParser from "body-parser";
+import express from "express";
+import clientRoutes from "./routes/clientRoutes.js";
+import swaggerSpec from "./api-docs.js";
+import swaggerUI from "swagger-ui-express";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use("/app/clients", clientRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+export default app;
